54cb64f1135b174ee43a2ee9473a750fcd966347
suryanshsharma1109-ops <suryanshsharma1109@gmail.com>
2026-04-22 22:24:22 +0530

    fixed render port config

--- p1.py
+++ p1.py
@@ -90,5 +90,8 @@ def run_code():
             "time": 0
         })
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
